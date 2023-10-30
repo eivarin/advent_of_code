be4414f2a6fcf740798863e753a182d21b7d9be0
eivarin <rodrigommpereira309@gmail.com>
2023-10-30 20:45:38 +0000

    day 14 done

--- 2022/day_14/gold.py
+++ 2022/day_14/gold.py
@@ -1,7 +1,55 @@
 day, year, part = (14, 2022, "b")
 
+m = set()
+last_height = 0
+
+def update_last_height(y):
+    global last_height
+    if y > last_height:
+        last_height = y
+
+def fall_sand():
+    global m, last_height
+    pos = (500,0)
+    while True:
+        if pos[1]+1 == last_height+2:
+            break
+        elif not (pos[0], pos[1]+1) in m:
+            pos = (pos[0], pos[1]+1)
+        elif not (pos[0]-1, pos[1]+1) in m:
+            pos = (pos[0]-1, pos[1]+1)
+        elif not (pos[0]+1, pos[1]+1) in m:
+            pos = (pos[0]+1, pos[1]+1)
+        else:
+            break
+    m.add(pos)
+    return (pos == (500,0))
+
 def solve(input):
-    return 0
+    global m, last_height
+    paths = [l.split(" -> ") for l in input.split("\n")]
+    for rocks in paths:
+        rocks = list(map(lambda x: (int(x[0]), int(x[1])), [r.split(",") for r in rocks]))
+        start_rock = rocks[0]
+        update_last_height(start_rock[1])
+        for end_rock in rocks[1:]:
+            update_last_height(end_rock[1])
+            rock_range = set()
+            if start_rock[0] == end_rock[0]:
+                bigger, smaller = (start_rock[1], end_rock[1]) if start_rock[1] > end_rock[1] else (end_rock[1], start_rock[1])
+                rock_range = set([(start_rock[0], ry) for ry in range(smaller, bigger+1)])
+            else:
+                bigger, smaller = (start_rock[0], end_rock[0]) if start_rock[0] > end_rock[0] else (end_rock[0], start_rock[0])
+                rock_range = set([(rx, start_rock[1]) for rx in range(smaller, bigger+1)])
+            m = m.union(rock_range)
+            start_rock = end_rock
+    i=1
+    while True:
+        if fall_sand():
+            break
+        else:
+            i+=1
+    return i
 
 import aocd
 def main():
