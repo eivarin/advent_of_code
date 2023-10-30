92b6b0fd801e93f5977b06647be201f64b5a9f02
eivarin <rodrigommpereira309@gmail.com>
2023-10-30 20:45:48 +0000

    day 15 done

--- 2022/day_15/gold.py
+++ 2022/day_15/gold.py
@@ -1,6 +1,38 @@
 day, year, part = (15, 2022, "b")
 
+import re
+manhatan_dist = lambda p1,p2: sum(abs(val1-val2) for val1, val2 in zip(p1,p2))
+
+drones = []
+
 def solve(input):
+    minc, maxc= (0,4000000)
+    possible_coords = set()
+    for l in input.splitlines():
+        x,y,cx,cy = [t(s) for t,s in zip((int,int,int,int),re.search('Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)',l).groups())]
+        new_drone = ((x,y),manhatan_dist((x,y),(cx,cy)))
+        drones.append(new_drone)
+    rx,ry = (0,0)
+    possible_coords = set()
+    for di, (coords, dist) in enumerate(drones):
+        print(f"Drone {di}")
+        maxy=coords[1]+dist
+        possible_coords = {(coords[0],coords[1]-dist-1), (coords[0],maxy+1)}
+        for yi,y in enumerate(range(coords[1]-dist, maxy+1)):
+            minx, maxx = (coords[0]-yi-1, coords[0]+yi+1)
+            possible_coords.add((minx,y))
+            possible_coords.add((maxx,y))
+        for c in possible_coords:
+            no_colision = True
+            for d in drones:
+                di = manhatan_dist(c,d[0])
+                if di<=d[1]:
+                    # print(f"Colision at {c} with {d}")
+                    no_colision = False
+                    break
+            if no_colision and c[0] > minc and c[0] < maxc and c[1] < maxc and c[1] > minc:
+                rx,ry = c
+                return rx*maxc+ry
     return 0
 
 import aocd
--- 2022/day_15/silver.py
+++ 2022/day_15/silver.py
@@ -1,11 +1,72 @@
 day, year, part = (15, 2022, "a")
 
+import re
+manhatan_dist = lambda p1,p2: sum(abs(val1-val2) for val1, val2 in zip(p1,p2))
+
+drones = [[],[],[]]
+
+
+magicrow = 2000000
+
 def solve(input):
-    return 0
+    minx, maxx = (0,4000000)
+    occupied_row = set()
+    beacons_row = set()
+    for l in input.splitlines():
+        x,y,cx,cy = [t(s) for t,s in zip((int,int,int,int),re.search('Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)',l).groups())]
+        new_drone = ((x,y),manhatan_dist((x,y),(cx,cy)))
+        if cy == magicrow:
+            beacons_row.add(cx)
+        if y<magicrow:
+            drones[0].append(new_drone)
+        elif y==magicrow:
+            drones[1].append(new_drone)
+        else:
+            drones[2].append(new_drone)
+    for d in drones[1]:
+        coords = d[0]
+        occupied_row.add(coords[0])
+        for x in range(coords[0]+1,coords[0]+d[1]+1):
+            occupied_row.add(x)
+        for x in range(coords[0]-d[1], coords[0]):
+            occupied_row.add(x)
+    for d in drones[0]:
+        coords = d[0]
+        dist_to_row=coords[1]+d[1]-magicrow
+        if dist_to_row > 0:
+            occupied_row.add(coords[0])
+            for x in range(coords[0]+1,coords[0]+dist_to_row+1):
+                occupied_row.add(x)
+            for x in range(coords[0]-dist_to_row, coords[0]):
+                occupied_row.add(x)
+    for d in drones[2]:
+        coords = d[0]
+        dist_to_row=magicrow-(coords[1]-d[1])
+        if dist_to_row > 0:
+            occupied_row.add(coords[0])
+            for x in range(coords[0]+1,coords[0]+dist_to_row+1):
+                occupied_row.add(x)
+            for x in range(coords[0]-dist_to_row, coords[0]):
+                occupied_row.add(x)
+    return len(occupied_row - beacons_row)
 
 import aocd
 def main():
     data = aocd.get_data(day=day, year=year)
+#     data = """Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+# Sensor at x=9, y=16: closest beacon is at x=10, y=16
+# Sensor at x=13, y=2: closest beacon is at x=15, y=3
+# Sensor at x=12, y=14: closest beacon is at x=10, y=16
+# Sensor at x=10, y=20: closest beacon is at x=10, y=16
+# Sensor at x=14, y=17: closest beacon is at x=10, y=16
+# Sensor at x=8, y=7: closest beacon is at x=2, y=10
+# Sensor at x=2, y=0: closest beacon is at x=2, y=10
+# Sensor at x=0, y=11: closest beacon is at x=2, y=10
+# Sensor at x=20, y=14: closest beacon is at x=25, y=17
+# Sensor at x=17, y=20: closest beacon is at x=21, y=22
+# Sensor at x=16, y=7: closest beacon is at x=15, y=3
+# Sensor at x=14, y=3: closest beacon is at x=15, y=3
+# Sensor at x=20, y=1: closest beacon is at x=15, y=3"""
     result = solve(data)
     print(f"============ Trying Submiting! ============\nCurrent Result: {result}")
     aocd.submit(result, day=day, year=year, part=part)
